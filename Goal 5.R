7fa4a0d346d9c3e973cb844b34dfcc10a15d15f5
khinechin <57419973+khinechin@users.noreply.github.com>
2019-11-11 20:17:46 -0500

    Update Goal 5.R

--- Goal 5.R
+++ Goal 5.R
@@ -1,77 +1,45 @@
-#Goal 4 & 5: Sarah Eyoas & Chin Chin
-
-#Goal 4 Cleaning the Data 
-#_____________________________________________________________________________________________________________________________________________
-#1. loading the readr dataset so that the files can be read into the program
-    library(dplyr)    
-    library(readr)
-    dataset <- read_csv("~/Data Science Folder/dataset.csv")
-
-#2. One is a dataset with informtion about the canidates, qty of hatecrimes, and he publicity each canidate recieved. 
-  #using the melt function we are trying to concatinate them into one colum (from "wide" to "long")
-    library(reshape2)
-    One = dataset %>% select(state, state_abbr,hate_crimes_per_100k_splc,`Rates of 2016`, `Rates of 2015`, `Rates of 2014`,
-                         Clinton..2016., Trump..2016.)
-    One_y <- One %>% melt(id = c("state", "state_abbr","hate_crimes_per_100k_splc", 'Rates of 2016', 'Rates of 2015', 'Rates of 2014'))
-
-#3. Two is a dataset with information about the total votes for each party.
-  #using melt again we are trying to concatinate the data into one colum (from "wide" to "long")
-    Two = dataset %>% select(state, votes_dem_2016, votes_gop_2016)
-    Two_y <- Two %>% melt(id = c("state"))
-
-#4. Final is a dataset with both One and Two altertered contents and column names that reflect what the data does. 
-Final = cbind(One_y, Two_y)
-colnames(Final)= c("State", "Abbr","Hate_Crimes", "Unemp2016", "Unemp2015", "Unemp2014", "Candidate", "Publicity",
-                   "State2","Party", "Votes" )
-#_____________________________________________________________________________________________________________________________________________
-                                #Goal 5 Mulipilating the data 
-##____________________________________________________________________________________________________________________________________________
-##PlOT 1- 
-Final2 <- Final %>% mutate(mean_unemp = (Unemp2016 + Unemp2015 + Unemp2014) / 3) %>% select(State, mean_unemp) 
-Final22=Final2 %>% group_by(State) %>% summarize(mean_unemp = mean(mean_unemp))%>% 
-  arrange(mean_unemp)
-##
-Final_V = Final[!is.na(Final$Votes),]
-Votes = Final_V %>% group_by(Party) %>% summarize(total_votes = sum(Votes))
-
-##
-Publicity <- Final %>% group_by(Candidate) %>% summarise(Average_Publicity = mean(Publicity))
+library(dplyr)
+library(readr)
 library(ggplot2)
-ggplot(Publicity, aes(x = Candidate, y = Average_Publicity, fill = Candidate)) + geom_bar(stat = "identity")
-##
-Rates = Final %>% select(Hate_Crimes, Unemp2016, Unemp2014, Unemp2015, Abbr)
-Rates = na.omit(Rates)
-
-Rates_ = Rates %>% select(Hate_Crimes, Unemp2016, Unemp2014, Unemp2015)
-k=kmeans(Rates_, centers=3, nstart= 25)
-Rates2 = Rates %>% mutate(clusters = factor(k$cluster))
+options(scipen = 99)
+dataset=read.csv("dataset.csv")
+datasetc= dataset%>%mutate(ratioC=votes_dem_2016/(votes_dem_2016 + votes_gop_2016))
+datasetct=datasetc %>%mutate(ratioT=votes_gop_2016/(votes_dem_2016 + votes_gop_2016))
 
 
+One=datasetct%>%select(state, state_abbr, hate_crimes_per_100k_splc, `Rates.of.2016`, `Rates.of.2015`, `Rates.of.2014`, `Clinton..2016.`, `Trump..2016.` )
+library(reshape2)                    
+One_y <- One %>% melt(id = c("state", "state_abbr","hate_crimes_per_100k_splc", 'Rates.of.2016', 'Rates.of.2015', 'Rates.of.2014'))
 
-ggplot(Rates2, aes(x = Hate_Crimes, y = Unemp2014, color = clusters, label = Abbr)) + 
-  geom_point()+geom_text(hjust = 0, vjust = 0)
+Two = datasetct %>% select(state, ratioC, ratioT)
+Two_y <- Two %>% melt(id = c("state"))
+
+Final=cbind(One_y, Two_y)
+colnames(Final)= c("State", "Abbr","Hate_Crimes", "Unemp2016", "Unemp2015", "Unemp2014", "Candidate", "Publicity",
+                   "State2","Ratio", "Votes" )
+Final=Final%>%mutate(Win = Votes > 0.5)
+ggplot(Final1, aes(x= Candidate,y= No_of_states, fill= Candidate))+geom_bar(stat="identity")
+
+Final1= Final%>%group_by(Candidate, Win)%>%summarise(n())
+colnames(Final1)=c("Candidate", "Win", "No_of_states")
+Final1= na.omit(Final1)
+Final1=subset(Final1, Win==TRUE)                        
+
+ggplot(Final1, aes(x= Candidate,y= No_of_states, fill= Candidate))+geom_bar(stat="identity")
+ggplot(Final,aes(x=Publicity, y=Votes, color=Candidate, label=Abbr))+geom_point()+geom_text(hjust=0, vjust=0)+ylab("Votes_Ratio")+ggtitle("Relationship between publicity and votes that each candidate received in each state")
+ggplot(Final, aes(x=Publicity, y=Votes, color=Candidate, label=Abbr))+geom_point()+geom_smooth(method=lm, se=FALSE)
+#Relationship between publicity and votes received.
+
+Final2 <- Final %>% mutate(mean_unemp = (Unemp2016 + Unemp2015 + Unemp2014) / 3) %>% select(State,Abbr, Unemp2016, Unemp2015, Unemp2014,mean_unemp,Candidate, Votes)
+Final2=na.omit(Final2)
+ggplot(Final2, aes(x=reorder(State,mean_unemp), y=Votes, fill= Candidate))+geom_bar(stat="identity",position = "dodge")+coord_flip()+xlab("Mean Unemployment(from highest to lowest)")+ylab("Votes Ratio")+ggtitle("Barplot to represent  how unemployment affects votes")
+                                                                                                                                                                                                                
+
+ggplot(Final2, aes(x=mean_unemp, y=Votes, color=Candidate, label=Abbr))+geom_point()+geom_text(hjust=0, vjust=0)+geom_line()
+ggplot(Final2, aes(x=mean_unemp, y=Votes, color=Candidate))+geom_point()+geom_smooth(method = lm, se = FALSE)
 
 
-##
-ratio <- dataset %>% mutate(ratio = votes_dem_2016/votes_gop_2016) %>% select(state, ratio)
-
-ratio_noDC = ratio %>% filter (state != "District of Columbia")
-ggplot(ratio, aes(x= state, y = ratio)) + geom_bar(stat = "identity") + coord_flip() + 
-  theme(text = element_text(size = 10))
-
-###DR morrison\
-
-##with DC
-ratio <- dataset %>% mutate(ratio = votes_dem_2016/votes_gop_2016) %>% select(state, ratio)
-ggplot(ratio, aes(x= reorder(state, ratio), y = ratio)) + 
-  geom_bar(stat = "identity",color="black", fill="skyblue") + coord_flip() + 
-  theme(text = element_text(size = 10))
-
-##NO  DC
-ggplot(ratio_noDC, aes(x= reorder(state, ratio), y = ratio)) + 
-  geom_bar(stat = "identity",color="black", fill="skyblue") + coord_flip() + 
-  theme(text = element_text(size = 10))
-
-
-##
-ggplot(Final, aes(x = Unemp2016, y = Votes/100000, color = Candidate)) + geom_point() + geom_smooth(method = lm, se = FALSE)
+Hate=Final%>%select(State, Abbr, Candidate, Hate_Crimes, Votes)
+Hate=Hate%>%filter(State!="District of Columbia")
+#ggplot(Hate, aes(x=Hate_Crimes, y=Votes, color=Candidate))+geom_point()+geom_text(hjust=0, vjust=0)+geom_smooth(method=lm, se=FALSE)
+ggplot(Hate, aes(x=Hate_Crimes, y=Votes, color=Candidate, label=Abbr))+geom_point()+geom_text(hjust=0,vjust=0)+geom_smooth(method = lm, se=FALSE)
