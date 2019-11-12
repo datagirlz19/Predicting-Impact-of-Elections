718ae3245f2801d84d74f0c674049af3ed6b3bf3
khinechin <57419973+khinechin@users.noreply.github.com>
2019-11-11 20:53:55 -0500

    Update Goal 5.R

--- Goal 5.R
+++ Goal 5.R
@@ -3,7 +3,6 @@ library(readr)
 library(ggplot2)
 options(scipen = 99)
 dataset=read.csv("dataset.csv")
-datasetc= dataset%>%mutate(ratioC=votes_dem_2016/(votes_dem_2016 + votes_gop_2016))
 datasetct=datasetc %>%mutate(ratioT=votes_gop_2016/(votes_dem_2016 + votes_gop_2016))
 
 
@@ -18,28 +17,24 @@ Final=cbind(One_y, Two_y)
 colnames(Final)= c("State", "Abbr","Hate_Crimes", "Unemp2016", "Unemp2015", "Unemp2014", "Candidate", "Publicity",
                    "State2","Ratio", "Votes" )
 Final=Final%>%mutate(Win = Votes > 0.5)
-ggplot(Final1, aes(x= Candidate,y= No_of_states, fill= Candidate))+geom_bar(stat="identity")
+Final_Win=Final%>%select(State, Abbr, Candidate, Publicity, Ratio, Votes, Win)
+Final_Win=na.omit(Final_Win)
 
 Final1= Final%>%group_by(Candidate, Win)%>%summarise(n())
 colnames(Final1)=c("Candidate", "Win", "No_of_states")
 Final1= na.omit(Final1)
-Final1=subset(Final1, Win==TRUE)                        
+Final1=subset(Final1, Win==TRUE)
+ggplot(Final1, aes(x= Candidate,y= No_of_states, fill= Candidate))+geom_bar(stat="identity")+ggtitle("The number of states each candidate won")
 
-ggplot(Final1, aes(x= Candidate,y= No_of_states, fill= Candidate))+geom_bar(stat="identity")
 ggplot(Final,aes(x=Publicity, y=Votes, color=Candidate, label=Abbr))+geom_point()+geom_text(hjust=0, vjust=0)+ylab("Votes_Ratio")+ggtitle("Relationship between publicity and votes that each candidate received in each state")
-ggplot(Final, aes(x=Publicity, y=Votes, color=Candidate, label=Abbr))+geom_point()+geom_smooth(method=lm, se=FALSE)
 #Relationship between publicity and votes received.
 
 Final2 <- Final %>% mutate(mean_unemp = (Unemp2016 + Unemp2015 + Unemp2014) / 3) %>% select(State,Abbr, Unemp2016, Unemp2015, Unemp2014,mean_unemp,Candidate, Votes)
 Final2=na.omit(Final2)
 ggplot(Final2, aes(x=reorder(State,mean_unemp), y=Votes, fill= Candidate))+geom_bar(stat="identity",position = "dodge")+coord_flip()+xlab("Mean Unemployment(from highest to lowest)")+ylab("Votes Ratio")+ggtitle("Barplot to represent  how unemployment affects votes")
-                                                                                                                                                                                                                
 
-ggplot(Final2, aes(x=mean_unemp, y=Votes, color=Candidate, label=Abbr))+geom_point()+geom_text(hjust=0, vjust=0)+geom_line()
-ggplot(Final2, aes(x=mean_unemp, y=Votes, color=Candidate))+geom_point()+geom_smooth(method = lm, se = FALSE)
 
 
 Hate=Final%>%select(State, Abbr, Candidate, Hate_Crimes, Votes)
 Hate=Hate%>%filter(State!="District of Columbia")
-#ggplot(Hate, aes(x=Hate_Crimes, y=Votes, color=Candidate))+geom_point()+geom_text(hjust=0, vjust=0)+geom_smooth(method=lm, se=FALSE)
-ggplot(Hate, aes(x=Hate_Crimes, y=Votes, color=Candidate, label=Abbr))+geom_point()+geom_text(hjust=0,vjust=0)+geom_smooth(method = lm, se=FALSE)
+ggplot(Hate, aes(x=Hate_Crimes, y=Votes, color=Candidate, label=Abbr))+geom_point()+geom_text(hjust=0,vjust=0)+geom_smooth(method = lm, se=FALSE)+ylab("Votes_Ratio")+ggtitle("Relationship between hate crimes and the voting ratio each candidate received in each state")
